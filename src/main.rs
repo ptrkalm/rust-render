accbafa43f23de96abc9397f2362759f3426fc59
ptrkalm <pkalmuczak@gmail.com>
2022-03-04 11:41:30 +0100

    changed binded port

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ async fn index() -> impl Responder {
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| App::new().service(index))
-        .bind("127.0.0.1:8080")?
+        .bind("0.0.0.0:8088")?
         .run()
         .await
 }
