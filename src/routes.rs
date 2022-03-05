e12294444093b80c9f141f43838c90fb52a92aab
ptrkalm <pkalmuczak@gmail.com>
2022-03-05 14:18:05 +0100

    added error returning

--- src/routes.rs
+++ src/routes.rs
@@ -10,7 +10,7 @@ pub async fn get_users(db: web::Data<Pool>) -> Result<HttpResponse, Error> {
     Ok(web::block(move || get_all_users(db))
         .await
         .map(|user| HttpResponse::Ok().json(user))
-        .map_err(|_| HttpResponse::InternalServerError())?)
+        .map_err(|err| HttpResponse::InternalServerError().json(format!("{:?}", err)))?)
 }
 
 fn get_all_users(pool: web::Data<Pool>) -> Result<Vec<User>, diesel::result::Error> {
